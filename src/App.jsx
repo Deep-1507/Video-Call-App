dc47099817597ad2914a464c4cbbbd5c946b2ede
Deep-1507 <deependrakumar15072003@gmail.com>
2024-08-18 22:23:13 +0530

    *

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
        <BrowserRouter>
         <Routes>
           
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/call" element={< Videocall />} />
           
         </Routes>
